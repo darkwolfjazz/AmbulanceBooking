9b3ede5ca4e88b3b85d1e679603aa3b91e9f4474
darkwolfjazz <1906349@kiit.ac.in>
2024-08-08 14:02:01 +0530

    Added loggers to project

--- src/main/java/com/ambulancebooking/project/controller/AmbulanceController.java
+++ src/main/java/com/ambulancebooking/project/controller/AmbulanceController.java
@@ -4,6 +4,8 @@ import com.ambulancebooking.project.entity.AmbulanceEntity;
 import com.ambulancebooking.project.service.AmbulanceService;
 import org.apache.logging.log4j.message.Message;
 import org.apache.tomcat.util.json.JSONParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +19,43 @@ import java.util.Optional;
 @RestController
 public class AmbulanceController {
 
+
+    Logger logger= LoggerFactory.getLogger(AmbulanceController.class);
+
+
+
 @Autowired
     private AmbulanceService ambulanceService;
 
  @PostMapping("/bookAmbulance")
    public ResponseEntity<AmbulanceEntity>bookAmbulanceforUser(@RequestBody AmbulanceEntity ambulanceEntity){
+     logger.info("Inside a post request");
      AmbulanceEntity bookedAmbulance=ambulanceService.bookAmbulance(ambulanceEntity);
      return new ResponseEntity<>(bookedAmbulance,HttpStatus.CREATED);
  }
 
  @GetMapping("/bookings")
     public List<AmbulanceEntity> getAllBookings(){
+     logger.info("Inside a get request, returns all bookings");
      return ambulanceService.seeAllBookings();
  }
 
  @GetMapping("/bookings/{id}")
     public Optional<AmbulanceEntity> getBookingById(@PathVariable Long id){
+     logger.info("Returning bookings by id");
      return ambulanceService.seeAllBookingsByID(id);
  }
 
  @DeleteMapping("/delete/{id}")
  public String DeleteRequestforAmbulance(@PathVariable Long id){
+     logger.info("deletes booking by id");
      ambulanceService.deleteById(id);
      return "Your booking is cancelled";
  }
 
  @PutMapping("/updateBooking/{id}")
     public String updateBookingById(@PathVariable Long id,@RequestBody AmbulanceEntity ambulanceEntity){
+     logger.info("Inside a put request");
     ambulanceService.updateambulanceentry(id, ambulanceEntity);
      return "Your booking is modified";
  }
--- src/main/java/com/ambulancebooking/project/service/AmbulanceService.java
+++ src/main/java/com/ambulancebooking/project/service/AmbulanceService.java
@@ -1,7 +1,10 @@
 package com.ambulancebooking.project.service;
 
+import com.ambulancebooking.project.controller.AmbulanceController;
 import com.ambulancebooking.project.entity.AmbulanceEntity;
 import com.ambulancebooking.project.repository.AmbulanceRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,33 +14,42 @@ import java.util.Optional;
 @Service
 public class AmbulanceService {
 
+
+    Logger logger= LoggerFactory.getLogger(AmbulanceService.class);
+
   @Autowired
     private AmbulanceRepository ambulanceRepository;
 
      public AmbulanceEntity bookAmbulance(AmbulanceEntity ambulanceEntity){
+
+         logger.info("AmbulanceService - bookAmbulance - starts" );
+
         if(ambulanceEntity.isAvailable()){
             ambulanceEntity.setStatus("Confirmed");
         }else{
             ambulanceEntity.setStatus("Pending as their is no availability");
         }
          ambulanceRepository.save(ambulanceEntity);
-         return ambulanceEntity;
+         logger.info("AmbulanceService - bookAmbulance - ends" );
+        return ambulanceEntity;
      }
 
      public List<AmbulanceEntity> seeAllBookings(){
+         logger.info("AmbulanceService - seeAllBookings " );
         return ambulanceRepository.findByAvailableTrue();
      }
 
      public Optional<AmbulanceEntity> seeAllBookingsByID(Long id){
+         logger.info("AmbulanceService - seeAllBookingsById - starts" );
          return ambulanceRepository.findById(id);
 
      }
-
      public void deleteById(Long id){
+         logger.info("AmbulanceService - deleteById - starts" );
          ambulanceRepository.deleteById(id);
      }
-
      public AmbulanceEntity updateambulanceentry(Long id, AmbulanceEntity updatedEntity){
+         logger.info("AmbulanceService - updateambulanceentry - starts" );
          Optional<AmbulanceEntity> existingEntity= ambulanceRepository.findById(id);
          if(existingEntity.isPresent()){
              AmbulanceEntity currentEntity=existingEntity.get();
@@ -50,6 +62,7 @@ public class AmbulanceService {
          }else {
              throw new RuntimeException();
          }
+         logger.info("AmbulanceService - updateambulanceentry - ends" );
          return updatedEntity;
      }
 }
